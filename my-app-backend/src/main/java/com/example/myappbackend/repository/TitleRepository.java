eadfa4da46db495764ca26552e8fc93377135f9e
Ryosuke2022053 <s2022053@stu.musashino-u.ac.jp>
2022-09-30 15:55:57 +0900

    [add]　レポジトリ層の作成

---
+++ my-app-backend/src/main/java/com/example/myappbackend/repository/TitleRepository.java
@@ -0,0 +1,9 @@
+package com.example.myappbackend.repository;
+
+import com.example.myappbackend.model.Title;
+
+import java.util.List;
+
+public interface TitleRepository {
+    List<Title> getTitle();
+}
